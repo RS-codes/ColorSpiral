95e1289e3658940d75d007369669d56ff0df9d3d
RS-codes <RS-codes@github.com>
2020-08-09 10:02:29 +0530

    14_ThankYou added

---
+++ 14_ThankYou.py
@@ -0,0 +1,5 @@
+#14_ThankYou.py
+your_name = input("What is your name? ")
+your_age = input("How old are you? ")
+print("Your name is",your_name,", and you are",your_age,"years old.")
+print("Thank you",your_name,"for spending ur valuable time!")
